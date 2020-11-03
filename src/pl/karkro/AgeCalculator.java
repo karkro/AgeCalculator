fddff818427f14a078c8215ef6607cc6c6d6f0a1
karkro <karol.krolikowski@gmail.com>
2020-11-03 16:00:39 +0100

    It's working. Add Scanner to input date of birth.

--- src/pl/karkro/AgeCalculator.java
+++ src/pl/karkro/AgeCalculator.java
@@ -1,8 +1,24 @@
 package pl.karkro;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
 public class AgeCalculator {
 
     public static void main(String[] args) {
-        System.out.println("test");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Year of birth: ");
+        int year = sc.nextInt();
+        System.out.print("Month of birth: ");
+        int month = sc.nextInt();
+        System.out.print("Day of birth: ");
+        int day = sc.nextInt();
+
+        LocalDate today  = LocalDate.now();
+        LocalDate birthday = LocalDate.of(year, month, day);
+        int years = Period.between(birthday, today).getYears();
+        System.out.println("You are " + years + " years old");
     }
 }
